ed2d056ad5d77e40752d01c644c6d02ddd194b3d
walid <bousnina.walid1@gmail.com>
2017-11-23 12:00:09 +0100

    change commentaire add walid in defaultcontroller.php branch walid

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        // replace this example code with whatever you  walid
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
